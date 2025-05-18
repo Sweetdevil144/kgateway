12b3ffa4e4f327657075e869e0079e73be68a068
Abhinav Pandey <abhinavpandey1230@gmail.com>
2025-05-18 19:42:06 +0530

    Create stopwatch_suite_test for executing tests
    Signed-off-by: Abhinav Pandey <abhinavpandey1230@gmail.com>

---
+++ pkg/utils/statsutils/stopwatch_suite_test.go
@@ -0,0 +1,12 @@
+package statsutils_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestStopWatchSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "StopWatch Suite")
+}
